693f12d3fd2cfe381b2beb97e4e20f37c86c18d8
Denis Balandin <doublestar2013@yandex.ru>
2023-12-19 08:43:17 +0300

    Initial Commit

---
+++ HotelBooking/HotelBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelBookingApp.swift
+//  HotelBooking
+//
+//  Created by Denis on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct HotelBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
